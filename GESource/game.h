1b8601957bf8317d09a5f0f17cd9e25c53fe961f
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2021-12-19 10:31:37 -0300

    update del engine con las slides

--- GESource/game.h
+++ GESource/game.h
@@ -4,11 +4,51 @@
 
 #ifndef GAMEENGINE2D_GAME_H
 #define GAMEENGINE2D_GAME_H
+#include "CommonHeaders.h"
+#include "Entity.h"
+#include "EntityManager.h"
 
+struct PlayerConfig {int SR,CR,FR,FG,FB,OR,OG,OB,OT,V; float S;};
+struct EnemyConfig  {int SR,CR,OR,OG,OB,OT,VMIN,VMAX,L,SI; float SMIN,SMAX;};
+struct BulletConfig {int SR,CR,FR,FG,FB,OR,OG,OB,OT,V,L; float S;};
 
 class game
 {
+    sf::RenderWindow m_window;
+    EntityManager   m_entities;
+    sf::Font    m_font;
+    sf::Text    m_text;
+    PlayerConfig    m_playerConfig;
+    EnemyConfig     m_enemyConfig;
+    BulletConfig    m_bulletConfig;
+    int             m_score=0;
+    int             m_currentFrame=0;
+    int             m_lastEnemySpawnTime =0;
+    bool            m_paused= false;
+    bool            m_running=true;
 
+    ptr<Entity> m_player;
+
+    void init(const std::string & config);
+    void setPaused(bool paused);
+
+    // Systems
+    void sMovement();
+    void sUserInput();
+    void sLifespan();
+    void sRender();
+    void sEnemySpawner();
+    void sCollision();
+
+    void spawnPlayer();
+    void spawnEnemy();
+    void SpawnSmallEnemies(ptr<Entity> entity);
+    void spawnBullet(ptr<Entity> entity, const Vec2 & mousePos);
+    void spawnSpecialWeapon(ptr<Entity> entity);
+
+public:
+    game(const std::string & config);
+    void run();
 };
 
 
